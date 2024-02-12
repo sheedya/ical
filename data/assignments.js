3e473bc96b75e0114ebd82d2ee3de2b35b21c58c
planit <planit@tanjeffreyz.github.io>
2024-02-12 11:32:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -349,6 +349,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Homework 15 - (H.3.I, H.3.J)",
+      "course": "ME 274 - (2:30-3:20 PM - Gibert Section)",
+      "dueDate": "2024-02-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/692071",
+      "submitted": false
+    },
+    {
       "title": "Quiz 1",
       "course": "ME 274 - (2:30-3:20 PM - Gibert Section)",
       "dueDate": "2024-01-12T15:25:00",
