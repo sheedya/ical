b001c696bfc1d04e2a1b05e082ab9106ae9680d3
planit <planit@tanjeffreyz.github.io>
2024-04-05 20:32:40 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -576,8 +576,8 @@ assignments = {
       "title": "Homework 32 - (H.5.K, H.5.L)",
       "course": "ME 274 - (2:30-3:20 PM - Gibert Section)",
       "dueDate": "2024-04-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/692071",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/692071/assignments/3897053/submissions/244424374",
+      "submitted": true
     },
     {
       "title": "Homework 33 - (H.5.M, H.5.N)",
