f1e27a93234675ebff56180a8f4acba0f2abc514
planit <planit@tanjeffreyz.github.io>
2024-08-18 16:37:31 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
   "Fall 2024 ME 30800 - Merge": [],
-  "ME32301": []
+  "ME32301": [
+    {
+      "title": "Syllabus Quiz",
+      "course": "ME32301",
+      "dueDate": "2024-08-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/826855/assignments/4748934/submissions/new",
+      "submitted": false
+    }
+  ]
 };
