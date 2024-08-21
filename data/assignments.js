93ca71a14a93d1319969d7718bcb051229a4142f
planit <planit@tanjeffreyz.github.io>
2024-08-21 16:39:50 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -24,6 +24,13 @@ assignments = {
       "dueDate": "2024-09-03T18:00:00",
       "link": "https://www.gradescope.com/courses/820474",
       "submitted": false
+    },
+    {
+      "title": "Prelab 1",
+      "course": "wl.202510.ME.36500.073",
+      "dueDate": "2024-08-26T07:00:00",
+      "link": "https://www.gradescope.com/courses/820474",
+      "submitted": false
     }
   ]
 };
