26687ac20b18072ca54da80b5ceee0d4a8d12391
planit <planit@tanjeffreyz.github.io>
2024-08-31 14:33:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -96,6 +96,13 @@ assignments = {
       "dueDate": "2024-09-10T18:00:00",
       "link": "https://www.gradescope.com/courses/820474",
       "submitted": false
+    },
+    {
+      "title": "Simulink HW1",
+      "course": "wl.202510.ME.36500.073",
+      "dueDate": "2024-09-24T18:00:00",
+      "link": "https://www.gradescope.com/courses/820474",
+      "submitted": false
     }
   ]
 };
