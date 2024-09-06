685928cc5469daf46ea7b25085daf5bbb599b87a
planit <planit@tanjeffreyz.github.io>
2024-09-06 11:34:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -47,7 +47,7 @@ assignments = {
     {
       "title": "Homework 2-Conceptual",
       "course": "ME 323-Fall 2024 (3:30section)-Kokini",
-      "dueDate": "2024-09-06T11:59:00",
+      "dueDate": "2024-09-06T23:59:00",
       "link": "https://www.gradescope.com/courses/821723/assignments/4852672/submissions/264671681",
       "submitted": true
     }
