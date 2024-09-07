94d377fd3b90a82739fe17c7b2a427b45ba58d97
planit <planit@tanjeffreyz.github.io>
2024-09-07 04:37:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,6 +20,13 @@ assignments = {
       "dueDate": "2024-09-06T23:59:00",
       "link": "https://www.gradescope.com/courses/823015/assignments/4839823/submissions/265085154",
       "submitted": true
+    },
+    {
+      "title": "HW03",
+      "course": "Fall 2024 ME 30800 - Merge",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/823015",
+      "submitted": false
     }
   ],
   "ME 323-Fall 2024 (3:30section)-Kokini": [
