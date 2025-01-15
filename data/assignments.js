188351584efec4473fef4451ccc67c03802279d9
planit <planit@tanjeffreyz.github.io>
2025-01-15 11:32:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -8,5 +8,14 @@ assignments = {
       "submitted": false
     }
   ],
-  "ME30801": []
+  "ME30801": [],
+  "wl.202520.ME.37500": [
+    {
+      "title": "HW1",
+      "course": "wl.202520.ME.37500",
+      "dueDate": "2025-01-21T18:00:00",
+      "link": "https://www.gradescope.com/courses/937531",
+      "submitted": false
+    }
+  ]
 };
