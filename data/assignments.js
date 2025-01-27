72d6be448564a3cc02a8d80445a03d5b1c1af32a
planit <planit@tanjeffreyz.github.io>
2025-01-27 14:37:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -37,6 +37,13 @@ assignments = {
       "dueDate": "2025-02-04T18:00:00",
       "link": "https://www.gradescope.com/courses/937531",
       "submitted": false
+    },
+    {
+      "title": "Lab 1",
+      "course": "wl.202520.ME.37500",
+      "dueDate": "2025-02-11T18:00:00",
+      "link": "https://www.gradescope.com/courses/937531",
+      "submitted": false
     }
   ]
 };
