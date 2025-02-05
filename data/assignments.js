1f9fb2b6ca09a546870767345cd7dae5ca707930
planit <planit@tanjeffreyz.github.io>
2025-02-05 22:38:15 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -18,8 +18,8 @@ assignments = {
       "title": "Quiz 01 (Boregowda)",
       "course": "ME 354",
       "dueDate": "2025-02-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/948547",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/948547/assignments/5664474/submissions/305477209",
+      "submitted": true
     },
     {
       "title": "Homework 3",
