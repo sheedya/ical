b908f285e5c573cf9ff2d87efb67b9928072c9c6
planit <planit@tanjeffreyz.github.io>
2025-02-09 21:32:56 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -31,13 +31,6 @@ assignments = {
   ],
   "ME30801": [
     {
-      "title": "Div 1: Wk06 - Lab 2 Report",
-      "course": "ME30801",
-      "dueDate": "2025-02-14T23:59:00",
-      "link": "https://www.gradescope.com/courses/946327",
-      "submitted": false
-    },
-    {
       "title": "Div 1: Wk05 - Lab 1 Report",
       "course": "ME30801",
       "dueDate": "2025-02-14T23:59:00",
