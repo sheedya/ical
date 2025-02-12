b494bdda824df53633ef82d687b80b829415e5ba
planit <planit@tanjeffreyz.github.io>
2025-02-12 23:32:53 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -53,8 +53,8 @@ assignments = {
       "title": "Exam 1 - Problem 2 resubmission",
       "course": "ME 354",
       "dueDate": "2025-02-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/948547",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/948547/assignments/5770125/submissions/307842748",
+      "submitted": true
     },
     {
       "title": "Exam 1 - Problem 3 resubmission",
