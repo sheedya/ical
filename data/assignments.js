65908f235465cf561ac6c1a06ed314c725df8dba
planit <planit@tanjeffreyz.github.io>
2025-02-18 01:55:53 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -69,6 +69,13 @@ assignments = {
       "dueDate": "2025-04-25T23:59:00",
       "link": "https://www.gradescope.com/courses/948547/assignments/5792417/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Homework 5",
+      "course": "ME 354",
+      "dueDate": "2025-02-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/948547",
+      "submitted": false
     }
   ],
   "ME30801": [
