b7a71276d49ad14e3fbb56de694a876052aba68b
planit <planit@tanjeffreyz.github.io>
2025-03-18 22:37:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -215,6 +215,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "HW5",
+      "course": "wl.202520.ME.37500",
+      "dueDate": "2025-03-25T18:00:00",
+      "link": "https://www.gradescope.com/courses/937531",
+      "submitted": false
+    },
+    {
       "title": "Exam 1 rework assignment",
       "course": "wl.202520.ME.37500",
       "dueDate": "2025-02-27T18:00:00",
