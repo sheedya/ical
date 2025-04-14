e596698a125394b326cfd7ccd49527324e1200f0
planit <planit@tanjeffreyz.github.io>
2025-04-14 06:46:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -316,8 +316,8 @@ assignments = {
       "title": "Prelab 6",
       "course": "wl.202520.ME.37500",
       "dueDate": "2025-04-14T08:30:00",
-      "link": "https://www.gradescope.com/courses/937531",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/937531/assignments/6048936/submissions/325102096",
+      "submitted": true
     }
   ]
 };
